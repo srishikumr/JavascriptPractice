5cd5686b22a97eea2c47b68c1e163e774206ca68
srishikumr <srishikumr@gmail.com>
2025-12-16 15:43:31 +0530

    save changes

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,5 +1,9 @@
 const todolist = [];
 
+let todoListHTML = '';
+
+let j = 0;
+
 function addTodo(){
     const name = document.querySelector('.js-name-input').value;
     todolist.push(name);
@@ -8,7 +12,18 @@ function addTodo(){
 }
 
 function addTodoPrint(){
-    
+    const name = document.querySelector('.js-name-input1').value;
+    todolist.push(name);
+    console.log(todolist);
+    //document.querySelector('.js-name-input').value = '';
+    for (let i = j; i<todolist.length; i++){
+        const todo = todolist[i];
+        const html = `<p>${todo}</p>`
+        todoListHTML += html;
+        j++;
+    }
+    console.log(todoListHTML);
+    document.querySelector('.todoListPrinter').innerHTML = todoListHTML;
 }
 
 function addTaskByEnter(event){

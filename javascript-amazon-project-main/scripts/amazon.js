0dc245802bb7bb546f1db30587d65989433486f7
srishikumr <srishikumr@gmail.com>
2026-02-13 20:30:27 +0530

    putting the cart quantity in page

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -90,6 +90,8 @@ products.forEach((product)=>{
               cartQuantity+= item.quantity  ;
             });
 
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
             console.log(cartQuantity);
 
             console.log(cart);
